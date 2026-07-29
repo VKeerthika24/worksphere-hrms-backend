33b1e7be0131238600aee0f4016b6102b1308d98
keerthikav24 <keerthikavk2406@gmail.com>
2026-07-29 20:31:40 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/worksphere/hrms/WorksphereHrmsApplication.java
@@ -0,0 +1,13 @@
+package com.worksphere.hrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksphereHrmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksphereHrmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worksphere/hrms/WorksphereHrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worksphere.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksphereHrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
