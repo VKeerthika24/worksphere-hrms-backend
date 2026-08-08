373e82fcfbe667cdb62302cfeed86aa562ee083a
VKeerthika24 <vkkeerthika24@gmail.com>
2026-08-08 11:53:06 +0530

    cors issue fixedz

--- src/main/java/com/worksphere/hrms/config/SecurityConfig.java
+++ src/main/java/com/worksphere/hrms/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session ->
